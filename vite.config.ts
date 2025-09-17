116793e3f3c8b66c240c4277e0093ce629c675ac
Jayming <wangjianming.x@gmail.com>
2025-09-17 15:21:28 +0800

    refactor: update vite.config.ts to load environment variables based on mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-  plugins: [react(), tailwindcss(), svgr()],
-}));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }],
+    },
+    plugins: [react(), tailwindcss(), svgr()],
+    server: {
+      proxy:
+        env.VITE_ENABLE_MOCK !== 'true'
+          ? {
+              '/api': {
+                target: env.VITE_API_BASE_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+              },
+            }
+          : undefined,
+    },
+  };
+});
