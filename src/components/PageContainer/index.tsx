409e3e2ab20bd5832d597f3a3b35c4be8957f4e7
Jayming <wangjianming.x@gmail.com>
2025-09-17 15:21:46 +0800

    refactor: enhance PageContainer component with semantic class handling and props

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,15 +1,25 @@
 import { use, type FC, type PropsWithChildren, type ReactNode } from 'react';
 import { HomeOutline } from '@metisjs/icons';
-import { Breadcrumb, type BreadcrumbProps } from 'metis-ui';
+import { Breadcrumb, clsx, type BreadcrumbProps, type SemanticClassName } from 'metis-ui';
+import useSemanticCls from 'metis-ui/es/_util/hooks/useSemanticCls';
 import { Link } from 'react-router';
 import MenuContext from '@/layouts/MainLayout/MenuConext';
 
 export interface PageContainerProps {
   title?: ReactNode;
+  className?: SemanticClassName<{ header?: string; content?: string }>;
+  ghost?: boolean;
 }
 
-const PageContainer: FC<PropsWithChildren<PageContainerProps>> = ({ title, children }) => {
+const PageContainer: FC<PropsWithChildren<PageContainerProps>> = ({
+  title,
+  children,
+  ghost,
+  className,
+}) => {
   const { matchedMenus, breadcrumbs } = use(MenuContext);
+
+  const semanticCls = useSemanticCls(className);
 
   const itemRender: BreadcrumbProps['itemRender'] = (item, _, __, ___, isLast) =>
     isLast ? (
@@ -27,14 +37,29 @@ const PageContainer: FC<PropsWithChildren<PageContainerProps>> = ({ title, child
   ];
 
   return (
-    <div>
-      <div className="mb-6 flex flex-wrap items-center justify-between gap-3">
+    <div className={semanticCls.root}>
+      <div
+        className={clsx(
+          'mb-6 flex flex-wrap items-center justify-between gap-3',
+          semanticCls.header,
+        )}
+      >
         <h2 className="text-text text-xl font-semibold">{title ?? currentMenu?.label}</h2>
         <Breadcrumb items={mergedBreadcrumbs} itemRender={itemRender} />
       </div>
-      <div className="rounded-2xl border border-gray-950/5 bg-white p-5 lg:p-6 dark:border-white/10 dark:bg-white/3">
-        {children}
-      </div>
+
+      {ghost ? (
+        <div className={semanticCls.content}>{children}</div>
+      ) : (
+        <div
+          className={clsx(
+            'rounded-2xl border border-gray-950/5 bg-white p-5 lg:p-6 dark:border-white/10 dark:bg-white/3',
+            semanticCls.content,
+          )}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
